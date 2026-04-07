a61df2ab437046ea774065382827dc391b2e80db
Abdullah Aloğlu <16511095+aloglu@users.noreply.github.com>
2026-04-07 15:41:47 +0300

    Fix release build tracking

---
+++ cmd/triage/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"github.com/aloglu/triage/internal/app"
+)
+
+func main() {
+	p := tea.NewProgram(app.New(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
